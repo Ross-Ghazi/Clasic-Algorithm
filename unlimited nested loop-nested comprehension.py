613b6af53bdec466a3f65c53ce7310397d94ed4b
Rouzbeh1797 <75285298+Rouzbeh1797@users.noreply.github.com>
2020-12-12 21:52:41 -0700

    Update unlimited nested loop-nested comprehension.py

--- unlimited nested loop-nested comprehension.py
+++ unlimited nested loop-nested comprehension.py
@@ -2,7 +2,9 @@
 #suppose length of n is a lot and you want to have all possble combinations:
 # ['adg', 'adh', 'adi', 'aeg',......
 # you cannot have lots of for loops inside each other especially if len(a) will change each time.
-# so there are two ways to solve that as shwon here:
+# so there are two ways to solve that as shwon here.
+# no reference but look at the video here:
+# https://leetcode.com/problems/letter-combinations-of-a-phone-number/solution/
 
 
 a=[["a","b","c"],["d","e","f"],["g","h","i"]]
