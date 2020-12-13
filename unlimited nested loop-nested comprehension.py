6ac113ac3364098668aea16cf73c62f13727c5e7
Rouzbeh1797 <75285298+Rouzbeh1797@users.noreply.github.com>
2020-12-12 21:50:08 -0700

    Update unlimited nested loop-nested comprehension.py

--- unlimited nested loop-nested comprehension.py
+++ unlimited nested loop-nested comprehension.py
@@ -1,7 +1,7 @@
 # By Rouzbeh on Dec 12
-#suppose length of n is al ot and you want to have all possble combinations:
+#suppose length of n is a lot and you want to have all possble combinations:
 # ['adg', 'adh', 'adi', 'aeg',......
-# you cannot have lots of for inside each other especially if len(a) will change each time.
+# you cannot have lots of for loops inside each other especially if len(a) will change each time.
 # so there are two ways to solve that as shwon here:
 
 
